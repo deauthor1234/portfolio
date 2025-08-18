ce252ea639c9bb482d2e9f200c3d3e4b3fa47fb1
Oladapo Authority <dimejioladapo573@gmail.com>
2025-08-18 11:38:45 +0100

    Develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
     return (
-      <div>
-        <h1 className="text-xl text-red-200">Portfolio Project</h1>
-      </div>
+      <>
+        <Navbar />
+      </>
     )
 }
 
