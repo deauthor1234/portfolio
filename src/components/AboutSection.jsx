051fc18234e50e536bcb1deb5184edee2bfc399e
Oladapo Authority <dimejioladapo573@gmail.com>
2025-08-25 01:21:37 +0100

    Styling and Making Responsive

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,10 +8,10 @@ const AboutSection = () => {
     return (
         <section className="container-wrapper" id="about">
             <div className="container flex mt-[6rem] justify-between items-center gap-16">
-                <div className="overflow-hidden rounded-l-4xl min-w-[30rem]" data-aos="fade-right">
+                <div className="max-lg:hidden overflow-hidden rounded-l-4xl size-full max-w-[30rem]" data-aos="fade-right">
                     <BiCodeCurly className="size-full" />
                 </div>
-                <div>
+                <div className="size-full">
                     <h2 className="section-heading" data-aos="fade-down">WHO AM I?</h2>
                     <p className="mt-6 mb-6" data-aos="fade" data-aos-delay="200">I am Oladapo Authority, I base in PortHarcourt, Rivers State, Nigeria. I am an ethusiastic frontend web developer with over 3 years of experience in building projects and learning to code. I build fully responsive and cross-browser compactible websites with modern design for small businesses and I can also work on frontend related projects.</p>
                     <h3 className="text-xl font-bold" data-aos="fade-right">Tech Stack & Tools:</h3>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,24 +4,24 @@ import { BiDownArrowAlt, BiLogoGithub, BiLogoInstagram, BiMailSend } from "react
 
 const Hero = () => {
     return (
-        <header className="container-wrapper relative h-[100vh] min-h-[40rem] bg-linear-to-b from-transparent to-gray-200" id="intro">
-            <div className="flex justify-between container gap-16 items-center">
-                <div>
-                    <h2 className="font-bold text-2xl" data-aos="fade-right">Hello!</h2>
-                    <h1 className="text-8xl font-extrabold mt-4 mb-6" data-aos="fade-left" data-aos-delay="200">I'm Authority</h1>
-                    <h3 className="font-bold text-4xl" data-aos="fade-right" data-aos-delay="400">Frontend Web Developer</h3>
-                    <h2 className="text-[1.1rem] mt-4 mb-14 font-medium italic" data-aos="fade-left" data-aos-delay="600">I craft modern, responsive web experiences that blend clean design with powerful functionality.</h2>
+        <header className="container-wrapper items-center relative h-[100vh] min-h-[40rem] bg-linear-to-b from-transparent to-gray-200" id="intro">
+            <div className="flex justify-between items-center container gap-10">
+                <div className="size-full">
+                    <h2 className="font-bold text-2xl max-[1200px]:text-xl" data-aos="fade-right">Hello!</h2>
+                    <h1 className="text-8xl font-extrabold mt-2 mb-6 max-[1200px]:text-7xl" data-aos="fade-left" data-aos-delay="200">I'm Authority</h1>
+                    <h3 className="font-bold text-4xl max-[1200px]:text-3xl" data-aos="fade-right" data-aos-delay="400">Front-End Web Developer</h3>
+                    <h2 className="text-[1.1rem] mt-6 mb-8 font-medium italic max-w-[80%] text-gray-800" data-aos="fade-left" data-aos-delay="600">I craft modern, responsive web experiences that blend clean design with powerful functionality.</h2>
                     <div className="flex font-extrabold text-[2.6rem] gap-6">
-                        <a href="https://www.instagram.com/authority.dev/" data-aos="zoom-in" data-aos-delay="800"><BiLogoInstagram /></a>
-                        <a href="https://github.com/deauthor1234" data-aos="zoom-in" data-aos-delay="1000"><BiLogoGithub /></a>
-                        <a href="mailto:dimejioladapo573@gmail.com" data-aos="zoom-in" data-aos-delay="1200"><BiMailSend /></a>
+                        <a className="group" href="https://www.instagram.com/authority.dev/" data-aos="zoom-in" data-aos-delay="800"><BiLogoInstagram className="group-hover:scale-115 duration-150" /></a>
+                        <a className="group" href="https://github.com/deauthor1234" data-aos="zoom-in" data-aos-delay="1000"><BiLogoGithub className="group-hover:scale-115 duration-150" /></a>
+                        <a className="group" href="mailto:dimejioladapo573@gmail.com" data-aos="zoom-in" data-aos-delay="1200"><BiMailSend className="group-hover:scale-115 duration-150" /></a>
                     </div>
                 </div>
-                <div className="min-w-[25rem] min-h-[25rem] rounded-r-4xl relative rotate-y-180 overflow-hidden">
-                    <img src={profile2} alt="profile" className="size-full object-cover scale-125 absolute" data-aos="zoom-out" />
+                <div className="min-w-[25rem] min-h-[25rem] rounded-r-4xl overflow-hidden relative rotate-y-180">
+                    <img src={profile2} alt="profile" className="size-full object-cover absolute scale-125" data-aos="zoom-out" />
                 </div>
             </div>
-            <Link to="about" smooth={true} className="animate-bounce cursor-pointer size-16 flex justify-center items-center shadow-2xl text-xl rounded-full absolute bg-white bottom-8 left-[50%] translate-x-[-50%]">
+            <Link to="about" smooth={true} className="animate-bounce cursor-pointer size-16 flex justify-center items-center shadow-2xl text-xl rounded-full absolute bg-white bottom-4 left-[50%] translate-x-[-50%]">
                 <BiDownArrowAlt className="text-3xl" />
             </Link>
         </header>
