a2f17fa1ab9a855b57ec7e7a1f45e5588e7dda28
Oladapo Authority <dimejioladapo573@gmail.com>
2025-08-29 21:29:51 +0100

    Tweak navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
                     <a href="/" className="font-extrabold text-[1.65rem]">dev-author.</a>
                 </div>
                 <BiMenu className="text-3xl cursor-pointer min-[769px]:hidden" onClick={() => !navVisibility ? setNavVisibility(true) : setNavVisibility(false)} />
-                <div className={`navbar max-[769px]:-z-10 max-[769px]:pointer-events-none flex gap-6 max-[769px]:border-b max-[769px]:border-gray-300 max-[769px]:gap-0 max-[769px]:absolute max-[769px]:top-[100%] max-[769px]:backdrop-blur-md max-[769px]:w-full max-[769px]:text-center max-[769px]:right-0 max-[769px]:flex-col ${navVisibility && "show"}`}>
+                <div className={`navbar max-[769px]:-z-10 max-[769px]:pointer-events-none bg-[#fff5] flex gap-6 max-[769px]:border-b max-[769px]:border-gray-300 max-[769px]:gap-0 max-[769px]:absolute max-[769px]:top-[100%] max-[769px]:backdrop-blur-md max-[769px]:w-full max-[769px]:text-center max-[769px]:right-0 max-[769px]:flex-col ${navVisibility && "show"}`}>
                 {sections.map(id => (
                     <Link
                     key={id}
