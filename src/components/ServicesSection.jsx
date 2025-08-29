43262c69fe6aa04fc4e74b2f17d6b5194eea43a5
Oladapo Authority <dimejioladapo573@gmail.com>
2025-08-29 21:05:48 +0100

    Make services section responsive

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -8,7 +8,7 @@ const ServicesSection = () => {
         <section id="services" className="container-wrapper mt-[6rem] py-24 bg-gray-100">
             <div className="container">
                 <h2 className="section-heading" data-aos="fade-right">WHAT CAN I DO?</h2>
-                <div className="grid grid-cols-3 max-[1025px]:grid-cols-2 gap-10 max-[1150px]:gap-6 mt-16 items-start mb-14">
+                <div className="grid grid-cols-3 max-[1025px]:grid-cols-2 max-[740px]:grid-cols-1 max-[740px]:gap-10 gap-10 max-[1150px]:gap-6 mt-16 items-start mb-14">
                     <Service delay="0" icon={<FaRegFileCode />} title="Custom Website Creation" description="I can create modern and fully responsive custom websites that work on all devices and browsers for small businesses (cafes, restaurants, salons, bakeries, etc), firms, events and Individuals (portfolio, online resume)." />
                     <Service delay="200" icon={<FaRecycle />} title="PSD & Figma Design Conversion" description="I have experience in converting PSD, Figma, XD and PDF designs to responsive functional websites or web pages." />
                     <Service delay="400" icon={<FaCode />} title="Front-End Web Development" description="Primarily, I am a frontend web developer, I can collaborate with other developers and designers to build more complex projects as a team." />
