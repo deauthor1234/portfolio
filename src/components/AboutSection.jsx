78c84bf16f11a27e60894b7f75c832bfbd7b41d6
Oladapo Authority <dimejioladapo573@gmail.com>
2025-12-24 13:53:40 +0100

    Classify skill set into diffect categories in AboutSection

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -13,19 +13,28 @@ const AboutSection = () => {
                 <div className="size-full">
                     <h2 className="section-heading" data-aos="fade-down">WHO AM I?</h2>
                     <p className="my-6" data-aos="fade" data-aos-delay="200">I am Oladapo Authority, I currently base in PortHarcourt, Rivers State, Nigeria. I am an ethusiastic frontend web developer with over 3 years of experience in building projects and learning to code. I build fully responsive and cross-browser compactible websites with modern design for small businesses and I can also work on frontend related projects.</p>
-                    <h3 className="text-xl font-bold" data-aos="fade-right">Tech Stack & Tools:</h3>
+                    <h3 className="text-xl font-bold" data-aos="fade-right">Languages:</h3>
                     <div className="flex mt-4 mb-8 font-medium about-tech-stack gap-4 flex-wrap">
                         <span data-aos="zoom-in" data-aos-delay="100"><BiLogoHtml5 /> Html5</span>
                         <span data-aos="zoom-in" data-aos-delay="200"><BiLogoCss3 /> Css3</span>
                         <span data-aos="zoom-in" data-aos-delay="300"><BiLogoJavascript /> JavaScript</span>
-                        <span data-aos="zoom-in" data-aos-delay="400"><BiLogoReact /> ReactJS</span>
                         <span data-aos="zoom-in" data-aos-delay="500"><BiLogoTypescript /> TypeScript</span>
+                    </div>
+                    <h3 className="text-xl font-bold" data-aos="fade-right">Frameworks:</h3>
+                    <div className="flex mt-4 mb-8 font-medium about-tech-stack gap-4 flex-wrap">
+                        <span data-aos="zoom-in" data-aos-delay="400"><BiLogoReact /> ReactJS</span>
                         <span data-aos="zoom-in" data-aos-delay="600"><BiLogoTailwindCss /> TailwindCSS</span>
+                    </div>
+                    <h3 className="text-xl font-bold" data-aos="fade-right">Design Tools:</h3>
+                    <div className="flex mt-4 mb-8 font-medium about-tech-stack gap-4 flex-wrap">
+                        <span data-aos="zoom-in" data-aos-delay="900"><BiLogoFigma /> Figma</span>
+                        <span data-aos="zoom-in" data-aos-delay="1100"><DiPhotoshop /> Adobe Photoshop</span>
+                    </div>
+                    <h3 className="text-xl font-bold" data-aos="fade-right">Deployment Tools:</h3>
+                    <div className="flex mt-4 mb-8 font-medium about-tech-stack gap-4 flex-wrap">
                         <span data-aos="zoom-in" data-aos-delay="700"><BiLogoGit /> Git</span>
                         <span data-aos="zoom-in" data-aos-delay="800"><BiLogoGithub /> Github</span>
-                        <span data-aos="zoom-in" data-aos-delay="900"><BiLogoFigma /> Figma</span>
                         <span data-aos="zoom-in" data-aos-delay="1000"><BiLogoNetlify /> Netlify</span>
-                        <span data-aos="zoom-in" data-aos-delay="1100"><DiPhotoshop /> Adobe Photoshop</span>
                     </div>
                     <div className="flex gap-4 max-[506px]:flex-col" data-aos="fade-up">
                         <Btn to="contact" icon={<FaEnvelope />} dark={true} content="Hire Me" />
