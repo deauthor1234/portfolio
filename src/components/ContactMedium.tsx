dbdc63a80c8208baaa44610d69bd76ac27847dc4
Oladapo Authority <dimejioladapo573@gmail.com>
2025-12-25 16:09:12 +0100

    ContactSection.jsx and ContactMedium.jsx

--- src/components/ContactMedium.tsx
+++ src/components/ContactMedium.tsx
@@ -1,6 +1,13 @@
 import { BiLinkExternal } from "react-icons/bi"
 
-const ContactMedium = ({ delay, href, icon, label }) => {
+type ContactProps = {
+  delay: string,
+  href: string,
+  icon: React.ReactNode,
+  label: string
+}
+
+const ContactMedium = ({ delay, href, icon, label }: ContactProps) => {
   return (
     <a className="contact-medium" onClick={() => window.open(href)} data-aos="zoom-in" data-aos-delay={delay}>
         {icon} 
